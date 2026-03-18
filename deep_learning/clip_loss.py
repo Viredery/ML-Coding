3ee391063042856ff78ae89ea7db8d1cc491d37a
Viredery <yiran_ding@outlook.com>
2026-03-18 23:12:31 +0800

    Remove num_ins in clip loss

--- deep_learning/clip_loss.py
+++ deep_learning/clip_loss.py
@@ -14,24 +14,21 @@ class ClipLoss(torch.nn.Module):
         Forward pass for the clip loss.
 
         Args:
-            image_embeds: torch.Tensor, shape (batch_size, num_ins, embedding_dim)
-            text_embeds: torch.Tensor, shape (batch_size, num_ins, embedding_dim)
+            image_embeds: torch.Tensor, shape (batch_size, embedding_dim)
+            text_embeds: torch.Tensor, shape (batch_size, embedding_dim)
 
         Returns:
             torch.Tensor, shape (1,)
         """
 
-        batch_size, num_instances, _ = image_embeds.shape
-
-        image_embeds = image_embeds.flatten(0, 1)
-        text_embeds = text_embeds.flatten(0, 1)
+        batch_size, _ = image_embeds.shape
 
         normed_image_embeds = image_embeds / (image_embeds.norm(dim=-1, keepdim=True) + 1e-8)
         normed_text_embeds = text_embeds / (text_embeds.norm(dim=-1, keepdim=True) + 1e-8)
 
         similarity = normed_image_embeds @ normed_text_embeds.transpose(0, 1)
         similarity = similarity * torch.exp(self.t)
-        labels = torch.arange(batch_size * num_instances, dtype=torch.long, device=image_embeds.device)
+        labels = torch.arange(batch_size, dtype=torch.long, device=image_embeds.device)
         
         image_loss = self.image_cross_entropy_loss(similarity, labels)
         text_loss = self.text_cross_entropy_loss(similarity.transpose(0, 1), labels)
@@ -40,7 +37,7 @@ class ClipLoss(torch.nn.Module):
 
 
 if __name__ == '__main__':
-    image_embeds = torch.randn(2, 3, 512)
-    text_embeds = torch.randn(2, 3, 512)
+    image_embeds = torch.randn(6, 512)
+    text_embeds = torch.randn(6, 512)
     loss = ClipLoss()
     print(loss(image_embeds, text_embeds))
